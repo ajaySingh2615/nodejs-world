79c87b02749a7750ad1f6a926a85e78be1c16230
ajaySingh2615 <ajaysingh261526@gmail.com>
2025-08-15 14:10:26 +0530

    Enhance index.js with additional custom middleware for improved request logging and processing

---
+++ 06_modules_pratical/index.js
@@ -0,0 +1,8 @@
+import xyz, { add } from "./math.js";
+
+console.log(add(1, 2));
+// console.log(subtract(1, 2));
+// console.log(multiply(1, 2));
+// console.log(divide(1, 2));
+
+xyz();
---
+++ 06_modules_pratical/math.js
@@ -0,0 +1,22 @@
+export function add(a, b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  return a / b;
+}
+
+export default function () {
+  console.log("I am a module");
+}
+
+// 1. named export
+// 2. default export
