8f98d0162cd75d2863f38ed0517ec809db2ebb68
ajaySingh2615 <ajaysingh261526@gmail.com>
2025-08-25 15:47:42 +0530

    Refactor App component structure and update header to reflect pizza menu branding

--- React-World/02-pizza-menu/src/index.js
+++ React-World/02-pizza-menu/src/index.js
@@ -48,12 +48,31 @@ const pizzaData = [
 
 function App() {
   return (
-    <>
-      <h1>Hello, React!</h1>
+    <div>
+      <Header />
+      <Menu />
+      <Footer />
+    </div>
+  );
+}
+
+function Header() {
+  return <h1>Fast React Pizza Co.</h1>;
+}
+function Menu() {
+  return (
+    <div>
+      <h2>Our Menu</h2>
       <Pizza />
       <Pizza />
       <Pizza />
-    </>
+    </div>
+  );
+}
+
+function Footer() {
+  return (
+    <footer>{new Date().toLocaleTimeString()} We're currently open!</footer>
   );
 }
 
