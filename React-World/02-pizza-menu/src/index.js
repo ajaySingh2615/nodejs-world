185fcd9fd5bf62a48ad8d9129e4a4098aa3e1e5c
ajaySingh2615 <ajaysingh261526@gmail.com>
2025-08-25 18:01:42 +0530

    Refactor Footer component to extract Order functionality into a separate component for improved readability and maintainability

--- React-World/02-pizza-menu/src/index.js
+++ React-World/02-pizza-menu/src/index.js
@@ -112,16 +112,24 @@ function Footer() {
   return (
     <footer className="footer">
       {isOpen ? (
-        <div className="order">
-          <p>We're open until {closeHour}:00. Come visit us or order online.</p>
-          <button className="btn">Order Now</button>
-        </div>
+        <Order closeHour={closeHour} />
       ) : (
         <p>
           We're happy to welcome between {openHour}:00 and {closeHour}:00.
         </p>
       )}
     </footer>
+  );
+}
+
+function Order(props) {
+  return (
+    <div className="order">
+      <p>
+        We're open until {props.closeHour}:00. Come visit us or order online.
+      </p>
+      <button className="btn">Order Now</button>
+    </div>
   );
 }
 
