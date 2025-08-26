4e9a260f39e38956267cff314167033c3410105d
ajaySingh2615 <ajaysingh261526@gmail.com>
2025-08-26 20:30:41 +0530

    feat: Update Stats component to display packing list statistics and handle empty state

--- React-World/04-travel-list/src/App.js
+++ React-World/04-travel-list/src/App.js
@@ -28,7 +28,7 @@ export default function App() {
         onDeleteItem={handleDeleteItem}
         onToggleItem={handleToggleItem}
       />
-      <Stats />
+      <Stats items={items} />
     </div>
   );
 }
@@ -112,10 +112,26 @@ function Item({ item, onDeleteItem, onToggleItem }) {
   );
 }
 
-function Stats() {
+function Stats({ items }) {
+  if (!items.length)
+    return (
+      <p className="stats">
+        <em>Start adding some items to your packing list!</em>
+      </p>
+    );
+
+  const numItems = items.length;
+  const numPacked = items.filter((item) => item.packed).length;
+  const packingPercentage = Math.round((numPacked / numItems) * 100);
+
   return (
     <footer className="stats">
-      <em>You have X items in your list, and you already packed X (X%)</em>
+      <em>
+        {packingPercentage === 100
+          ? "You have got everything packed! Ready to go!"
+          : `You have ${numItems} items in your list, and you already packed
+        ${numPacked} (${packingPercentage}%)`}
+      </em>
     </footer>
   );
 }
